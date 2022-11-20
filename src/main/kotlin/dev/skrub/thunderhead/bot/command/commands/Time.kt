056c9d52cf501bdd502a9bdfe9809293420820f8
vivi <38266782+humboldt123@users.noreply.github.com>
2022-11-19 23:46:19 -0500

    move request to its own function

--- src/main/kotlin/dev/skrub/thunderhead/bot/command/commands/Time.kt
+++ src/main/kotlin/dev/skrub/thunderhead/bot/command/commands/Time.kt
@@ -7,15 +7,13 @@ import dev.minn.jda.ktx.messages.MessageCreate
 import dev.skrub.thunderhead.bot.command.Command
 import dev.skrub.thunderhead.bot.command.Type
 import dev.skrub.thunderhead.bot.info.Color
+import dev.skrub.thunderhead.bot.util.Utils
 import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData
 import org.json.JSONObject
-import java.net.HttpURLConnection
-import java.net.URL
 import java.time.ZoneId
 import java.time.ZonedDateTime
-import java.util.Scanner
 
 class Time : Command(
     name = "time",
@@ -26,47 +24,34 @@ class Time : Command(
     override fun execute(event: GenericCommandInteractionEvent) {
         val location = event.getOption<String>("location")!!
         try {
-            val url = URL(
-                "https://geocoding-api.open-meteo.com/v1/search?name=${
-                    location.replace(" ", "+").replace("&", "")
-                }&format=json"
-            )
-            val connection = url.openConnection() as HttpURLConnection
-            connection.requestMethod = "GET"
-            connection.connect()
-            val responseCode = connection.responseCode
-            if (responseCode == 200) {
-                var collector = ""
-                val scanner = Scanner(url.openStream())
+            val response = JSONObject(Utils.request("https://geocoding-api.open-meteo.com/v1/search?name=${
+                location.replace(" ", "+").replace("&", "")
+            }&format=json"))
 
-                while (scanner.hasNext()) collector += scanner.nextLine()
-                scanner.close()
-
-                val jsonObject = JSONObject(collector)
-
-                if (!jsonObject.has("results")) {
-                    event.reply("I could not find the time in `$location`. Are you sure it exists?").queue()
-                } else {
-                    val result = jsonObject.getJSONArray("results").getJSONObject(0)
-                    val date = ZonedDateTime.now(ZoneId.of(result.getString("timezone"))).toLocalDateTime()
-                    // TODO: debating adding a footer with the chinese calendar year like in scythe
-                    event.reply(MessageCreate {
-                        embeds += Embed {
-                            title = "${result.getString("name")}, ${result.getString("admin1")}"
-                            field {
-                                name = "Day"
-                                value = "${date.dayOfWeek.name.lowercase().replaceFirstChar { it.uppercase() }}, ${date.month.name.lowercase().replaceFirstChar { it.uppercase() }} ${date.dayOfMonth}"
-                                inline = true
-                            }
-                            field {
-                                name = "Time"
-                                value = String.format("`%d:%02d`", date.hour, date.minute)
-                                inline = true
-                            }
-                            color = Color.discord
+            if (!response.has("results")) {
+                event.reply("I could not find the time in `$location`. Are you sure it exists?").queue()
+            } else {
+                val result = response.getJSONArray("results").getJSONObject(0)
+                val date = ZonedDateTime.now(ZoneId.of(result.getString("timezone"))).toLocalDateTime()
+                // TODO: debating adding a footer with the chinese calendar year like in scythe
+                event.reply(MessageCreate {
+                    embeds += Embed {
+                        title = "${result.getString("name")}, ${result.getString("admin1")}"
+                        field {
+                            name = "Day"
+                            value = "${
+                                date.dayOfWeek.name.lowercase().replaceFirstChar { it.uppercase() }
+                            }, ${date.month.name.lowercase().replaceFirstChar { it.uppercase() }} ${date.dayOfMonth}"
+                            inline = true
                         }
-                    }).queue()
-                }
+                        field {
+                            name = "Time"
+                            value = String.format("`%d:%02d`", date.hour, date.minute)
+                            inline = true
+                        }
+                        color = Color.discord
+                    }
+                }).queue()
             }
         } catch (e: Exception) {
             event.reply("An error occurred while trying to find the time in `$location`")
